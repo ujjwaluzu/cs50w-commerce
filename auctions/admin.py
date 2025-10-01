f89e649811ad02ef4e1b5b766fc38bd19b967753
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-10-01 14:20:14 +0530

    models done

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User, Category
+from .models import User, Category, Listing, Bid, Comment
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Category)
+admin.site.register(Listing)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Max
 
 
 class User(AbstractUser):
@@ -19,8 +20,9 @@ class Listing(models.Model):
     image = models.URLField(max_length=100, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Listings")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     created_at = models.DateTimeField(auto_now_add=True)
+    watchlisted_by = models.ManyToManyField(User, related_name="watchlist", blank=True)
 
     def __str__(self):
         return self.title
@@ -28,16 +30,23 @@ class Listing(models.Model):
     def no_of_bids(self):
         return self.bids.all().count()
     def current_price(self):
-        if self.no_of_bids > 0:
-            return round(self.bids.aggregate(Max("amount"))["amount_max"])
+        if self.no_of_bids() > 0:
+            return round(self.bids.aggregate(Max("amount"))["amount__max"])
         else:
             return self.start_bid
-
+    def is_in_watchlist(self, user):
+        return user.watchlist.filter(pk=self.pk).exists()
     
-class bid(models.Model):
+    class Meta:
+        ordering = ['-created_at']
+    
+class Bid(models.Model):
     item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
     amount = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return str(self.amount)
 class Comment(models.Model):
     item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
@@ -45,4 +54,7 @@ class Comment(models.Model):
     time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.comment
+        return str(self.comment)
+
+    class Meta:
+        ordering = ['-time']
