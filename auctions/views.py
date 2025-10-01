60560caef4e938149c4aa82ad41b6876e5d4a87f
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-10-01 14:44:10 +0530

    Working on new listing

--- auctions/views.py
+++ auctions/views.py
@@ -61,3 +61,6 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def New_listing(request):
+    return render(request, "auctions/newlisting.html")
