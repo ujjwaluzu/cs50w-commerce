9fa69f27123d41c0dac194d7c96ba755f08ac144
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-10-02 12:09:27 +0530

    working on new listing page

--- auctions/views.py
+++ auctions/views.py
@@ -3,8 +3,9 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, Category, Comment, Listing, Bid
 
 
 def index(request):
@@ -63,4 +64,15 @@ def register(request):
         return render(request, "auctions/register.html")
 
 def New_listing(request):
-    return render(request, "auctions/newlisting.html")
+    return render(request, "auctions/newlisting.html", {
+        "form": NewPageform()
+    })
+
+
+class NewPageform(forms.Form):
+    title = forms.CharField(label="Title", widget=forms.TextInput(attrs={'class': 'form-control w-75 mb-2'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control w-75 mb-2', 'rows': 3}), label="Description",)
+    start_bid = forms.DecimalField(label="Start bid",max_digits=10,decimal_places=2,min_value=0.01,widget=forms.NumberInput(attrs={'class': 'form-control w-75 mb-2', 'step': '0.01'}))
+    image_url = forms.URLField(label="Image URL", widget=forms.URLInput(attrs={'class': 'form-control w-75 mb-2'}))
+    category = forms.ModelChoiceField(label="category", queryset=Category.objects.all(),  widget=forms.Select(attrs={'class': 'form-control w-75 mb-2'}))
+    
