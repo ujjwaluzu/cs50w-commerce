dd9ba04de8e340cfcac4402a05dc6bb686c9d54c
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-10-06 14:51:15 +0530

    working on new create new listing backend

--- auctions/views.py
+++ auctions/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
+from django.shortcuts import redirect
 
 from .models import User, Category, Comment, Listing, Bid
 
@@ -66,15 +67,27 @@ def register(request):
         return render(request, "auctions/register.html")
 
 def New_listing(request):
+    if request.method == "POST":
+        form = NewPageform(request.POST)
+        if form.is_valid():
+            listing = form.save(commit=False)
+            listing.author = request.user
+            listing.save()
+            return redirect('index')
+    else:
+        form = NewPageform()
     return render(request, "auctions/newlisting.html", {
-        "form": NewPageform()
+        "form": form
     })
 
 
-class NewPageform(forms.Form):
+class NewPageform(forms.ModelForm):
     title = forms.CharField(label="Title", widget=forms.TextInput(attrs={'class': 'form-control w-75 mb-2'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control w-75 mb-2', 'rows': 3}), label="Description",)
     start_bid = forms.DecimalField(label="Start bid",max_digits=10,decimal_places=2,min_value=0.01,widget=forms.NumberInput(attrs={'class': 'form-control w-75 mb-2', 'step': '0.01'}))
-    image_url = forms.URLField(label="Image URL", widget=forms.URLInput(attrs={'class': 'form-control w-75 mb-2'}))
+    image = forms.URLField(label="Image URL", widget=forms.URLInput(attrs={'class': 'form-control w-75 mb-2'}))
     category = forms.ModelChoiceField(label="category", queryset=Category.objects.all(),  widget=forms.Select(attrs={'class': 'form-control w-75 mb-2'}))
     
+    class Meta:
+        model = Listing
+        fields = ['title', 'description', 'start_bid', 'image', 'category']
