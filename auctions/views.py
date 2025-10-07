71c4165a1737f427bb609d89147e759ce3fa1168
Ujjwal Baunthiyal <baunthiyalujjwal52@gmail.com>
2025-10-07 14:29:12 +0530

    fixed few bugs and some styling changes also made it responsive

--- auctions/views.py
+++ auctions/views.py
@@ -86,7 +86,7 @@ class NewPageform(forms.ModelForm):
     title = forms.CharField(label="Title", widget=forms.TextInput(attrs={'class': 'form-control w-75 mb-2'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control w-75 mb-2', 'rows': 3}), label="Description",)
     start_bid = forms.DecimalField(label="Start bid",max_digits=10,decimal_places=2,min_value=0.01,widget=forms.NumberInput(attrs={'class': 'form-control w-75 mb-2', 'step': '0.01'}))
-    image = forms.URLField(label="Image URL", widget=forms.URLInput(attrs={'class': 'form-control w-75 mb-2'}))
+    image = forms.URLField(label="Image URL", required=False, widget=forms.URLInput(attrs={'class': 'form-control w-75 mb-2'}))
     category = forms.ModelChoiceField(label="category", queryset=Category.objects.all(),  widget=forms.Select(attrs={'class': 'form-control w-75 mb-2'}))
     
     class Meta:
